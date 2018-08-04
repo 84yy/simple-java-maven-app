99834184780c43baa7985894d35fb43e570286cc
Simon <baxi84yy@163.com>
2018-08-04 20:39:42 +0800

    .

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker ps
+docker info
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
